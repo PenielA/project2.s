2f06e80dded187d5ea4b1f2e66c60d3e857121de
PenielA <36523791+PenielA@users.noreply.github.com>
2018-11-16 14:53:08 -0500

    The first line .data holds all the data of the program. null error message, length error message, base error message are the names of the outputs to be displayed accordingly. the word .assciz stores the string in the data segment and adds null terminator. the 5th line which is user input .space 1000 reserves the next 1000 bytes in the data segment.

---
+++ mipsfile2.asm
@@ -0,0 +1,5 @@
+.data 
+   null_errorMessage:   .asciiz  "Input is empty."
+   length_errorMessage:   .asciiz  "Input is too long."
+   base_errorMessage:   .asciiz   "Invalid base-31 number."
+   user_Input:   .space 1000
