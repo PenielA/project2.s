61da17f264d91c0a7914ae56b2a8da90a18cedfe
PenielA <36523791+PenielA@users.noreply.github.com>
2018-11-16 18:13:42 -0500

    The syscall on line 11 tells the system to execute. the next line which is Removespace infront is just a label that indicates the next algorithm is to remove space infront. the next instruction saves the immediate space character which holds the decimal value 32 In the ascii table. the next instruction loads the first index(byte) from the input into t7.

--- mipsfile2.asm
+++ mipsfile2.asm
@@ -1,10 +1,14 @@
-.data 
+   .data 
    null_errorMessage:   .asciiz  "Input is empty."
    length_errorMessage:   .asciiz  "Input is too long."
    base_errorMessage:   .asciiz   "Invalid base-31 number."
    user_Input:   .space 1000
-.text
+   .text
 main:
    li $v0, 8    
    la $a0, user_Input  
    li $a1, 1000
+   syscall 
+removespaceinfront:
+   li $t8, 32                              
+   lb $t7, 0($a0)   
