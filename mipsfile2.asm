c54ee061fd01946953714471736eb93f15aa40d5
PenielA <36523791+PenielA@users.noreply.github.com>
2018-11-16 15:58:41 -0500

    On the 6th line there is .text that has all the instructions that will be used to write the program. the next line is main which determines where to start the program taking the first label.  the next line after main is signaling the system to read(load) in a string. the next line is simply loading the user input and putting it in a0. li a1 is telling the system the maximum characters a user is allowed to enter.

--- mipsfile2.asm
+++ mipsfile2.asm
@@ -3,3 +3,8 @@
    length_errorMessage:   .asciiz  "Input is too long."
    base_errorMessage:   .asciiz   "Invalid base-31 number."
    user_Input:   .space 1000
+.text
+main:
+   li $v0, 8    
+   la $a0, user_Input  
+   li $a1, 1000
